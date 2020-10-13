4cf67ee07e600679b9195cfb2fd454c8a16f64fc
Viliam Simko <viliam.simko@gmail.com>
2020-10-13 17:59:06 +0200

    fix compilation for node 12 - use Nan::To<?> macro - use isolate

--- src/node_openalpr.cc
+++ src/node_openalpr.cc
@@ -19,9 +19,12 @@ using namespace v8;
 uv_mutex_t 		listMutex;
 bool running = false;
 
-class LPRQueueItem 
+v8::Isolate::CreateParams create_params;
+v8::Isolate* isolate = v8::Isolate::New(create_params);
+
+class LPRQueueItem
 {
-	public:	
+	public:
 		char *path;
 		char *state;
 		std::string prewarp;
@@ -37,26 +40,26 @@ class LPR {
 			this->openalpr->setTopN (10);
 			this->config = this->openalpr->getConfig ();
 		}
-		
+
 		~LPR () {
 			delete this->openalpr;
 		}
-		
+
 		bool isLoaded () {
 			return this->openalpr->isLoaded ();
 		}
-		
+
 		alpr::AlprResults recognize (LPRQueueItem *queueItem) {
 			this->openalpr->setDefaultRegion (queueItem->state);
 			this->openalpr->setDetectRegion (queueItem->detectRegion);
 			this->config->prewarp = queueItem->prewarp;
-			
+
 			std::ifstream ifs (queueItem->path, std::ios::binary|std::ios::ate);
 			std::ifstream::pos_type pos = ifs.tellg ();
 			std::vector<char>  buffer(pos);
 			ifs.seekg(0, std::ios::beg);
 			ifs.read(&buffer[0], pos);
-			
+
 			if (queueItem->regions.size ()) {
 				return this->openalpr->recognize (buffer, queueItem->regions);
 			}
@@ -64,22 +67,22 @@ class LPR {
 				return this->openalpr->recognize (buffer);
 			}
 		}
-		
+
 		bool isWorking () {
 			return working;
 		}
-		
+
 		void isWorking (bool working) {
 			this->working = working;
 		}
-		
+
 	private:
 		alpr::Alpr *openalpr;
 		alpr::Config *config;
 		bool working = false;
 };
 
-class LPRWorker : public Nan::AsyncWorker 
+class LPRWorker : public Nan::AsyncWorker
 {
 	public:
 		LPRWorker (LPR *lpr, LPRQueueItem *queueItem) : Nan::AsyncWorker (queueItem->callback) {
@@ -87,31 +90,31 @@ class LPRWorker : public Nan::AsyncWorker
 			this->lpr->isWorking (true);
 			this->queueItem = queueItem;
 		}
-		
+
 		~LPRWorker () {
 			delete this->queueItem;
 		}
-		
+
 		void Execute () {
 			alpr::AlprResults results = lpr->recognize (queueItem);
 			output = alpr::Alpr::toJson (results);
 		}
-		
+
 		void HandleOKCallback () {
 			Nan::HandleScope scope;
 			v8::Local<v8::Value> argv[] = {
 				Nan::Null (),
 				Nan::New<String>(output.c_str ()).ToLocalChecked ()
 			};
-			
+
 			lpr->isWorking (false);
 			if (!running) {
 				delete lpr;
 			}
-			
+
 			callback->Call (2, argv);
 		}
-		
+
 	private:
 		LPR *lpr;
 		LPRQueueItem *queueItem;
@@ -124,12 +127,12 @@ static std::list<LPRQueueItem *> queue;
 char *get (v8::Local<v8::Value> value)
 {
 	if (value->IsString ()) {
-		v8::String::Utf8Value string (value);
+		v8::String::Utf8Value string (isolate, value);
 		char *str = (char *)malloc (string.length () + 1);
 		strcpy (str, *string);
 		return str;
 	}
-	
+
 	return "";
 }
 
@@ -138,15 +141,15 @@ NAN_METHOD (Start)
 	running = true;
 	char *config_path = get (info[0]);
 	char *runtime_path = get (info[1]);
-	int instancesCount = info[2]->NumberValue ();
-	
+	int instancesCount = Nan::To<int32_t>(info[2]).FromMaybe(0);
+
 	// Create a list of instances, if any fail to load return false
 	for (int i = 0; i < instancesCount; i++) {
 		LPR *lpr = new LPR (config_path, runtime_path);
 		if (lpr->isLoaded () == false) {
 			info.GetReturnValue ().Set (false);
 		}
-		
+
 		instances.push_back (lpr);
 	}
 
@@ -162,17 +165,17 @@ NAN_METHOD (Stop)
 	while (queue.size () > 0) {
 		LPRQueueItem *item = queue.front ();
 		delete item;
-		
+
 		queue.pop_front ();
 	}
-	
+
 	instances.clear ();
 	info.GetReturnValue ().Set (true);
 }
 
 NAN_METHOD (GetVersion)
 {
-	std::string version = alpr::Alpr::getVersion ();	
+	std::string version = alpr::Alpr::getVersion ();
 	info.GetReturnValue ().Set <String> (Nan::New <String> (version).ToLocalChecked ());
 }
 
@@ -181,24 +184,25 @@ NAN_METHOD (IdentifyLicense)
 	// Lock our mutex so we can safetly modify our lists
 	uv_mutex_lock (&listMutex);
 
-	// Settings	
+	// Settings
 	char *path = get (info[0]);
 	char *state = get (info[1]);
 	char *prewarp = get (info[2]);
-	bool detectRegion = info[3]->BooleanValue ();
+	bool detectRegion = Nan::To<bool>(info[3]).FromMaybe(false);
+
 	Local<Array> regionsArray = info[4].As<Array> ();
 	Nan::Callback *callback = new Nan::Callback (info[5].As<Function>());
-	
+
 	std::vector<alpr::AlprRegionOfInterest> regions;
 	for (int i = 0; i < regionsArray->Length (); i++) {
 		Local<Array> regionValues = Local<Array>::Cast (regionsArray->Get (i));
-		int x = regionValues->Get (0)->Uint32Value ();
-		int y = regionValues->Get (1)->Uint32Value ();
-		int width = regionValues->Get (2)->Uint32Value ();
-		int height = regionValues->Get (3)->Uint32Value ();
+		int x =       Nan::To<uint32_t>(regionValues->Get (0)).FromMaybe(0);
+		int y =       Nan::To<uint32_t>(regionValues->Get (1)).FromMaybe(0);
+		int width =   Nan::To<uint32_t>(regionValues->Get (2)).FromMaybe(0);
+		int height =  Nan::To<uint32_t>(regionValues->Get (3)).FromMaybe(0);
 		regions.push_back (alpr::AlprRegionOfInterest (x, y, width, height));
 	}
-		
+
 	LPRQueueItem *item = new LPRQueueItem ();
 	item->path = path;
 	item->state = state;
@@ -206,21 +210,21 @@ NAN_METHOD (IdentifyLicense)
 	item->detectRegion = detectRegion;
 	item->regions = regions;
 	item->callback = callback;
-	
+
 	for (auto &i : instances) {
 		if (!i->isWorking ()) {
 			Nan::AsyncQueueWorker (new LPRWorker (i, item));
-			
+
 			// Unlock mutexes - we're done changing the lists
 			uv_mutex_unlock (&listMutex);
 			info.GetReturnValue ().Set <String> (Nan::New <String> ("working").ToLocalChecked ());
 			return;
 		}
 	}
-	
+
 	// If we reach here, we need to queue the image for later processing
 	queue.push_back (item);
-	
+
 	// Unlock mutexes - we're done changing the lists
 	uv_mutex_unlock (&listMutex);
 	info.GetReturnValue ().Set <String> (Nan::New <String> ("queued").ToLocalChecked ());
@@ -229,27 +233,27 @@ NAN_METHOD (IdentifyLicense)
 NAN_METHOD (CheckQueue)
 {
 	int started = 0;
-	
+
 	uv_mutex_lock (&listMutex);
-	
+
 	// Verify we have a queue, then start trying to associate them with an instance
 	if (queue.size () > 0) {
 		for (auto &i : instances) {
 			if (!i->isWorking ()) {
 				LPRQueueItem *item = queue.front ();
 				Nan::AsyncQueueWorker (new LPRWorker (i, item));
-				
+
 				// Delete items from the front for a FIFO operation
 				queue.pop_front ();
 				started++;
-				
+
 				if (queue.size () == 0) {
 					break;
 				}
 			}
 		}
 	}
-	
+
 	uv_mutex_unlock (&listMutex);
 	info.GetReturnValue ().Set (started);
 }
